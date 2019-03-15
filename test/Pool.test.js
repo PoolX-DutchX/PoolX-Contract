6f033e96b6e108a9c8f8f2bb512ee235c91e6105
Timo Hedke <lucky_expert@t-online.de>
2019-03-15 17:01:17 +0100

    Test need still refactoring with new version

--- test/Pool.test.js
+++ test/Pool.test.js
@@ -4,26 +4,22 @@ const EtherToken = artifacts.require('./EtherToken.sol')
 const DutchExchangeProxy = artifacts.require('./DutchExchangeProxy.sol')
 const DutchExchange = artifacts.require('./DutchExchange.sol')
 const PoolXCloneFactory = artifacts.require('./PoolXCloneFactory.sol')
-const BigNumber = web3.BigNumber
 
 const { increaseTimeTo, duration } = require('./helpers/timer')
+const { BN, balance, constants, expectEvent, shouldFail, ether, should } = require('openzeppelin-test-helpers');
 
-// eslint-disable-next-line
-const should = require('chai')
-  .use(require('chai-as-promised'))
-  .use(require('chai-bignumber')(BigNumber))
-  .should()
 
-contract('Pool', ([owner]) => {
-  let pool, clonedPool, poolCloneFactory, token, weth, dx
-  const initialClosingPriceNum = 2
-  const initialClosingPriceDen = 1
-
+contract('Pool', ([owner, contributor1, contributor2]) => {
+    let pool, clonedPool, poolCloneFactory, token, weth, dx, dutchX
+    const initialClosingPriceNum = 2
+    const initialClosingPriceDen = 1
+    const oneEth = ether('1');
   beforeEach(async () => {
-    //workaround, see https://github.com/ethereum/web3.js/issues/2077
     token = await Token.new(web3.utils.toWei("100000000000000000000"))
     weth = await EtherToken.deployed()
     dx = await DutchExchangeProxy.deployed()
+
+    dutchX = DutchExchange.at(dx.address)
     pool = await Pool.new()
     await pool.init(
       dx.address,
@@ -33,48 +29,62 @@ contract('Pool', ([owner]) => {
       initialClosingPriceDen
     )
 
-    poolCloneFactory = await PoolXCloneFactory.deployed(pool.address)
+    // poolCloneFactory = await PoolXCloneFactory.deployed(pool.address)
 
-    clonedPool = await poolCloneFactory
-      .createPool(
-        dx.address,
-        weth.address,
-        token.address,
-        initialClosingPriceNum,
-        initialClosingPriceDen
-      )
-      .then(tx => Pool.at(tx.logs[0].args.newPoolAddress))
+    // clonedPool = await poolCloneFactory
+    //   .createPool(
+    //     dx.address,
+    //     weth.address,
+    //     token.address,
+    //     initialClosingPriceNum,
+    //     initialClosingPriceDen
+    //   )
+    //   .then(tx => Pool.at(tx.logs[0].args.newPoolAddress))
 
     // await token.transfer(pool.address, 10e18);
   })
 
   describe('#contribute', () => {
     it('should deposit ether in the contract', async () => {
-      await pool.contribute({ from: owner, value: 1e18 })
-
-      const poolBalance = await web3.eth.getBalance(pool.address)
-      poolBalance.should.be.bignumber.eq(1e18)
+      await pool.contribute(0, 0, { from: contributor1, value: oneEth })
+      const contributedAmountToken1 = await pool.contributorAmountToken1(contributor1)
+      const poolBalance = await balance.current(pool.address)
+      contributedAmountToken1.should.be.bignumber.eq(oneEth)
+      poolBalance.should.be.bignumber.eq(oneEth)
     })
 
-    it('should update the pool funds in USD ', async () => {
-      await pool.contribute({ from: owner, value: 1e18 })
+    it.only('should deposit weth token in the contract', async () => {
 
-      const poolBalanceInUsd = await pool.getBalanceInUsd()
-      poolBalanceInUsd.should.be.bignumber.eq(1100e18)
+        await weth.deposit({ from: contributor1, value: oneEth })
+
+        await weth.approve(pool.address, oneEth)
+
+        await pool.contribute(oneEth, 0, { from: contributor1})
+        // const contributedAmountToken1 = await pool.contributorAmountToken1(contributor1)
+        // contributedAmountToken1.should.be.bignumber.eq(oneEth)
+        // const auctionIndex = await dx2.getAuctionIndex(weth.address, token.address)
+        // auctionIndex.should.be.bignumber.eq(0)
+     })
+
+    it('should update the pool funds in USD ', async () => {
+      await pool.contribute(0, 0, { from: contributor1, value: oneEth })
+
+      const poolBalanceInUsd = await pool.getEthInUsd()
+
+      poolBalanceInUsd.should.be.bignumber.eq(1100)
+
+     
     })
 
     it('should be able to list the token', async () => {
-      await clonedPool.contribute({
-        from: owner,
-        value: 10e18,
-      })
+        await weth.deposit({ from: contributor1, value: 10e18 })
+
+        await weth.approve(pool.address, 10e18,{ from: contributor1})
+        await pool.contribute(10e18,0,{ from: contributor1 })
 
       // no more ether in pool contract
       const poolBalance = web3.eth.getBalance(pool.address)
       poolBalance.should.be.bignumber.eq(0)
-
-      // need to get dutchX from DutchExchange
-      const dutchX = DutchExchange.at(dx.address)
 
       const auctionListedIndex = await dutchX.getAuctionIndex(
         weth.address,
@@ -84,6 +94,29 @@ contract('Pool', ([owner]) => {
       auctionListedIndex.should.be.bignumber.eq(1)
     })
   })
+
+  describe('#withdraw', () => {
+    beforeEach(async () => {
+        await pool.contribute(0,0,{
+          from: contributor1,
+          value: oneEth,
+        })
+      })
+      it('should withdraw weth token from contract', async () => {
+        const contributor1BalanceBefore = await web3.eth.getBalance(contributor1)
+
+        await pool.withdraw({ from: contributor1 })
+        const contributedAmountToken1 = await pool.contributorAmountToken1(contributor1)
+        contributedAmountToken1.should.be.bignumber.eq(0)
+        const contributedAmountToken2 = await pool.contributorAmountToken2(contributor1)
+        contributedAmountToken2.should.be.bignumber.eq(0)
+        const contributor1BalanceAfter = await web3.eth.getBalance(contributor1)
+        contributor1BalanceAfter.should.be.bignumber.eq(contributor1BalanceBefore.plus(oneEth))
+
+
+     })
+  })
+
   describe('#collectFunds', () => {
     beforeEach(async () => {
       await clonedPool.contribute({

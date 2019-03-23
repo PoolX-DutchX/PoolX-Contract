5c25d20a44b03f0591c534906269fe4d416d0a8d
Hendrik Bilges <elektronaut0815@users.noreply.github.com>
2019-03-23 22:21:55 +0100

    defining 'should'

--- test/Pool.test.js
+++ test/Pool.test.js
@@ -6,6 +6,7 @@ const DutchExchange = artifacts.require('./DutchExchange.sol')
 // const PoolXCloneFactory = artifacts.require('./PoolXCloneFactory.sol')
 
 const { duration } = require('./helpers/timer')
+const should = require('chai').should()
 const {
   BN,
   balance,

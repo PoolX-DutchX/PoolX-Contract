2e0711292d14e134cd19c268bda12093a65e091e
Timo Hedke <lucky_expert@t-online.de>
2019-03-23 11:33:25 +0100

    Fixed "should withdraw eth token from contract"

--- test/Pool.test.js
+++ test/Pool.test.js
@@ -157,9 +157,8 @@ contract('Pool', ([owner, contributor1]) => {
       )
       contributedTokenAmount2.should.be.bignumber.eq('0')
 
-      contributor1BalanceBefore
-        .sub(gasCosts)
-        .should.be.bignumber.eq(contributor1BalanceAfter)
+      contributor1BalanceBefore.sub(gasCosts)
+        .should.be.bignumber.eq(contributor1BalanceAfter.sub(oneEth))
     })
 
     it('should withdraw weth from contract', async () => {

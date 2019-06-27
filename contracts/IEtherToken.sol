00a3411ce82fc6ef41a2158753c3ce8272d6a7f9
Hendrik Bilges <elektronaut0815@users.noreply.github.com>
2019-06-27 14:11:27 +0200

    removing 'node_modules' from import paths

--- contracts/IEtherToken.sol
+++ contracts/IEtherToken.sol
@@ -1,7 +1,7 @@
 
 pragma solidity ^0.5.2;
 
-import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
 /// @title A token for wrapping Ether
 contract IEtherToken is ERC20 {
@@ -11,7 +11,7 @@ contract IEtherToken is ERC20 {
 
     /// @dev Buys tokens with Ether, exchanging them 1:1.
     function deposit() external payable;
-   
+
     /// @dev Sells tokens in exchange for Ether, exchanging them 1:1.
     /// @param amount Number of tokens to sell.
     function withdraw(uint amount) external;
